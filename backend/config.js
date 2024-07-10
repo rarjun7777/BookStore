72accc3df19ce3ba4275d98bb4813877bd3cc1f1
rarjun7777 <ramavatharjun2003@gmail.com>
2024-07-10 15:08:51 +0530

    BookStore

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://arjun_123:arjun123@cluster0.fpuyxx0.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0";
